f3f7bfa3522eb7271f066043b8e7f6be96c19209
amirkfir <amirkfir93@gmail.com>
2024-11-07 10:27:57 +0100

    robot control based movment worked on robot 10 (34753)

---
+++ robot_control.py
@@ -0,0 +1,46 @@
+import dynamixel_sdk as dxl
+ADDR_MX_TORQUE_ENABLE = 24
+ADDR_MX_CW_COMPLIANCE_MARGIN = 26
+ADDR_MX_CCW_COMPLIANCE_MARGIN = 27
+ADDR_MX_CW_COMPLIANCE_SLOPE = 28
+ADDR_MX_CCW_COMPLIANCE_SLOPE = 10
+ADDR_MX_GOAL_POSITION = 30
+ADDR_MX_MOVING_SPEED = 32
+ADDR_MX_PRESENT_POSITION = 50
+ADDR_MX_PUNCH = 48
+PROTOCOL_VERSION = 1.0
+DXL_IDS = [1,2,3,4]
+ADDR_CW_ANGLE_LIMIT = 6
+ADDR_CCW_ANGLE_LIMIT = 8
+DEVICENAME = "/dev/ttyACM0"
+BAUDRATE = 1000000
+TORQUE_ENABLE = 1
+TORQUE_DISABLE = 0
+portHandler = dxl.PortHandler(DEVICENAME)
+packetHandler = dxl.PacketHandler(PROTOCOL_VERSION)
+portHandler.openPort()
+portHandler.setBaudRate(BAUDRATE)
+
+max_Values = [[0,800],[130,650],[480,1024],[200,600]]
+
+for DXL_ID in DXL_IDS:
+    dxl_comm_result, dxl_error = packetHandler.write1ByteTxRx(portHandler, DXL_ID, ADDR_MX_TORQUE_ENABLE, TORQUE_ENABLE)
+    dxl_comm_result, dxl_error = packetHandler.write2ByteTxRx(portHandler, DXL_ID, ADDR_MX_CW_COMPLIANCE_MARGIN, 0)
+    dxl_comm_result, dxl_error = packetHandler.write2ByteTxRx(portHandler, DXL_ID, ADDR_CW_ANGLE_LIMIT, max_Values[DXL_ID-1][0])
+    dxl_comm_result, dxl_error = packetHandler.write2ByteTxRx(portHandler, DXL_ID, ADDR_CCW_ANGLE_LIMIT, max_Values[DXL_ID-1][1])
+
+    # dxl_comm_result, dxl_error = packetHandler.write2ByteTxRx(portHandler, DXL_ID, ADDR_MX_CCW_COMPLIANCE_MARGIN, 0)
+    # dxl_comm_result, dxl_error = packetHandler.write2ByteTxRx(portHandler, DXL_ID, ADDR_MX_CW_COMPLIANCE_SLOPE, 32)
+    # dxl_comm_result, dxl_error = packetHandler.write2ByteTxRx(portHandler, DXL_ID, ADDR_MX_CCW_COMPLIANCE_SLOPE, 32)
+    dxl_comm_result, dxl_error = packetHandler.write2ByteTxRx(portHandler, DXL_ID, ADDR_MX_MOVING_SPEED, 100)
+
+for DXL_ID in DXL_IDS:
+    for i in range (max_Values[DXL_ID-1][0],max_Values[DXL_ID-1][1]):
+        dxl_comm_result, dxl_error = packetHandler.write1ByteTxRx(portHandler, DXL_ID, ADDR_MX_TORQUE_ENABLE, 0)
+        dxl_comm_result, dxl_error = packetHandler.write4ByteTxRx(portHandler, DXL_ID, ADDR_MX_GOAL_POSITION,
+                                                                 i)
+        dxl_comm_result, dxl_error = packetHandler.write1ByteTxRx(portHandler, DXL_ID, ADDR_MX_TORQUE_ENABLE, 0)
+
+
+
+portHandler.closePort()
